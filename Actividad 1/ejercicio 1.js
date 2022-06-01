ee2c24624002637b019e7a323453761a00e8ea61
Alvaro del Angel <nachodelangel96@gmail.com>
2022-05-31 21:43:22 -0500

    Creacion de actividades

---
+++ Actividad 1/ejercicio 1.js
@@ -0,0 +1,22 @@
+//Ejercicio 1
+numero = prompt ( "Ingresa un valor numerico" )
+resultado = numero * 2
+consola _ registro ( resultado )
+
+//Ejercicio 2
+numero1 = indicador ( "Primer numero" )
+numero2 = indicador ( "Segundo numero" )
+resta = numero1 - numero2
+alerta ( resta )
+
+//Ejercicio 3
+var  r = 5.3
+constante  pi = 3.14
+areaCirculo = pi * ( r ** 2 )
+consola _ registro ( areaCirculo )
+
+//Ejercicio 4
+constante  D = 8
+constante  d = 6
+areaRombo = ( D * d ) / 2
+consola _ registro ( areaRombo )
