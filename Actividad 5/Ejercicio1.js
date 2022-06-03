ef8e7f48bf2301c4fe8127e191055337c770709f
Alvaro <80017045+alsada96@users.noreply.github.com>
2022-06-02 22:09:29 -0500

    Add files via upload
    Agregando los 3 primeros ejercicios de la actividad 5

---
+++ Actividad 5/Ejercicio1.js
@@ -0,0 +1,15 @@
+var ejercicio1 = [1,4,6,10,22,55,46,2,5,0];
+
+for (i=0; i <= ejercicio1.length; i++){
+    console.log(ejercicio1[i]);
+}
+
+//Ejercicio 1.1
+//Imprime los valores que sean mayores a tres
+var ejercicio1 = [1,4,6,10,22,55,46,2,5,0];
+
+for (i=1; i <= 9; i++){
+    if(ejercicio1[i] > 3){
+        console.log(ejercicio1[i]);
+    }
+}
---
+++ Actividad 5/Ejercicio2.js
@@ -0,0 +1,7 @@
+var ejercicio1 = [1,4,6,10,22,55,46,2,5,0];
+var vacio=[];
+
+while(vacio.length < 5){
+    vacio.push(ejercicio1.sort()[vacio.length])
+}
+console.log(vacio);
