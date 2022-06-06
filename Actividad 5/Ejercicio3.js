c271def1f6fbe78f46f6f5d1652755254662d437
Alvaro <80017045+alsada96@users.noreply.github.com>
2022-06-05 23:03:00 -0500

    Delete Ejercicio3.js

--- Actividad 5/Ejercicio3.js
+++
@@ -1,6 +0,0 @@
-var ejercicio3 = vacio.sort(function(a, b){return a - b}); //replace by b-a to change the order
-for (let i = 0; i < ejercicio3.length; i++) 
-{
-    console.log(ejercicio3[i]);
-    
-}
